049e070aade15057b7e34ca9e507c999d1dd7873
Luis <114909465+luiskcc@users.noreply.github.com>
2025-10-16 00:44:36 +0200

    added features section

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -4,6 +4,7 @@ import { PricingSection } from "@/components/home/PricingSection"
 import { FAQSection } from "@/components/home/FAQSection"
 import { CTASection } from "@/components/home/CTASection"
 import { HeroSection } from "@/components/home/HeroSection"
+import { FeaturesSection } from "@/components/home/FeaturesSection"
 import { CommunitySection } from "@/components/home/CommunitySection"
 
 export const metadata: Metadata = {
@@ -15,6 +16,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center overflow-x-hidden">
       <HeroSection />
+      <FeaturesSection />
       <CommunitySection />
       <TestimonialsSectionLoader />
       <PricingSection />
